4699ee99e5026710c3922402f8c603e611009862
Subramaniya Nivash P <snivash@tekion.com>
2022-08-19 12:12:47 +0530

    comma added in the correct ansers array of objects

--- src/correctAnswers.js
+++ src/correctAnswers.js
@@ -1,101 +1,102 @@
 export const correctAnswers = [
     {
         QuestionNumber:'1',
-        Ans:'Narmada'
+        Ans:'Narmada',
     },
     {
         QuestionNumber:'2',
-        Ans:'Cuttack'
+        Ans:'Cuttack',
     },
     {
         QuestionNumber:'3',
-        Ans:'Panini'
+        Ans:'Panini',
     },
     {
         QuestionNumber:'4',
-        Ans:'Bhagirathi'
+        Ans:'Bhagirathi',
     },
     {
         QuestionNumber:'5',
-        Ans:'Silver'
+        Ans:'Silver',
     },
     {
         QuestionNumber:'6',
-        Ans:'Australia'
+        Ans:'Australia',
     },
     {
         QuestionNumber:'7',
-        Ans:'None of these'
+        Ans:'None of these',
     },
     {
         QuestionNumber:'8',
-        Ans:'East Indian Cricket Club'
+        Ans:'East Indian Cricket Club',
     },
     {
-        QuestionNumber:'9',Ans:'England'
+        QuestionNumber:'9',
+        Ans:'England',
     },
     {
         QuestionNumber:'10',
-        Ans:'Pakistan'
+        Ans:'Pakistan',
     },
     {
         QuestionNumber:'11',
-        Ans:'Yuvraj Singh'
+        Ans:'Yuvraj Singh',
     },
     {
         QuestionNumber:'12',
-        Ans:'Russia'
+        Ans:'Russia',
     },
     {
         QuestionNumber:'13',
-        Ans:['Ravindra Jadeja','Mahendra Singh Dhoni']
+        Ans:['Ravindra Jadeja','Mahendra Singh Dhoni'],
     },
     {
         QuestionNumber:'14',
-        Ans:'Hurricanes'
+        Ans:'Hurricanes',
     },
     {
         QuestionNumber:'15',
-        Ans:'Ganga basin'
+        Ans:'Ganga basin',
     },
     {
         QuestionNumber:'16',
-        Ans:'Mosley'
+        Ans:'Mosley',
     },
     {
         QuestionNumber:'17',
-        Ans:'Caspian sea'
+        Ans:'Caspian sea',
     },
     {
         QuestionNumber:'18',
-        Ans:['Chennai Super Kings','Kolkata Knight Riders']
+        Ans:['Chennai Super Kings','Kolkata Knight Riders'],
     },
     {
         QuestionNumber:'19',
-        Ans:'Nepal'
+        Ans:'Nepal',
     },
     {
         QuestionNumber:'20',
-        Ans:'Venus'
+        Ans:'Venus',
     },
     {
         QuestionNumber:'21',
-        Ans:'Dehradun'
+        Ans:'Dehradun',
     },
     {
         QuestionNumber:'22',
-        Ans:'Infrastructure development'
+        Ans:'Infrastructure development',
     },
     {
         QuestionNumber:'23',
-        Ans:'Godavari'
+        Ans:'Godavari',
     },
     {
         QuestionNumber:'24',
-        Ans:'Chamera'
+        Ans:'Chamera',
     },
     {
         QuestionNumber:'25',
-        Ans:'Federation of Football Council'
+        Ans:'Federation of Football Council',
     }
 ]
